1e09f12ae79101bd860b09dc4f4f7cc5368f7d5d
killme <killme@users.noreply.github.com>
2014-06-20 11:40:13 +0200

    Better argument parsing, add silent option.

--- luapp.lua
+++ luapp.lua
@@ -2,75 +2,81 @@ local luvit = require "luvit.init"
 local native = require "uv_native"
 local process = require "luvit.process"
 local traceback = require "debug".traceback
+local table = require "table"
 
 local origionalP = p
 p = function() end
 
 local function printUsage()
-    process.stderr:write(("Usage: %s [-i | -r | -d] [-v] <script file> [<Arguments>...]\n"):format(process.argv[0]))
+    process.stderr:write(("Usage: %s [-i | -r | -d] [-v] [-s] [--] <script file> [<Arguments>...]\n"):format(process.argv[0]))
     process.exit(1)
 end
 
 if not process.argv[1] then
     printUsage()
 else
+    local flags = {}
+    local args = {}
+
+    local parsingArgs = false
+    for k, v in pairs(process.argv) do
+        if v ~= process.argv[0] then
+            parsingArgs = parsingArgs or (v:sub(1, 1) ~= '-')
+            if parsingArgs then
+                table.insert(args, v)
+            else
+                parsingArgs = v == '--'
+                if not parsingArgs then
+                    flags[v] = v
+                end
+            end
+        end
+    end
+    
+    if flags["-v"] then
+        p = origionalP
+    end
+
     local loadFunction = require
-    local skipArgs = 1
-
-    if process.argv[1]:sub(1, 1) == '-' then
-        skipArgs = 2
-        if process.argv[1] == '-i' then
-            loadFunction = nil
-        elseif process.argv[1] == '-d' then
-            loadFunction = _G.dofile
-        elseif process.argv[1] == '-r' then
-        else
-            return printUsage()
-        end
+    if flags["-i"] then
+        loadFunction = nil
+    elseif flags["-d"] then
+        loadFunction = dofile
     end
-
-    if process.argv[2] and process.argv[2]:sub(1, 1) == '-' then
-        if process.argv[2] == '-v' then
-            skipArgs = 3
-            p = origionalP
-        end
-    end
-
     if loadFunction ~= nil then
         process.luappArgv = process.argv
 
         local newArgs = {}
-        for k, v in pairs(process.argv) do
-            if k > skipArgs then
-                newArgs[k-skipArgs] = v
-            elseif newArgs[0] then
-                newArgs[0] = newArgs[0] .. " " .. v
-            else
-                newArgs[0] = v
-            end
+        for k, v in ipairs(args) do
+            newArgs[k-1] = v
         end
         
         process.argv = newArgs
 
         assert(xpcall(function()
-            loadFunction(process.luappArgv[skipArgs])
+            loadFunction(newArgs[0])
         end, traceback))
     else
+        local isSilent = flags["-s"] == "-s"
         p = origionalP -- Always verbose
-        process.stdout:write("lua>");
+        if not isSilent then
+            process.stdout:write("lua>");
+        end
         process.stdin:on("data", function(data)
             local suc, err = pcall(function() 
                 local func = loadstring("p("..data..")")
                 if not func then
                     func = assert(loadstring(data))
                 end
-                p(func())
+                func()
             end)
             
             if not suc then
                 process.stderr:write(err.."\n")
             end
-            process.stdout:write("lua>");
+            if not isSilent then
+                process.stdout:write("lua>");
+            end
         end)
         process.stdin:readStart()
     end
